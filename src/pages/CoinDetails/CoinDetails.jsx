e516806dadbc5cd4f64d10e4645100404b03007d
hieptrinh96 <xmikecolx@gmail.com>
2022-12-15 22:48:32 -0600

    saving coin data in progress

--- src/pages/CoinDetails/CoinDetails.jsx
+++ src/pages/CoinDetails/CoinDetails.jsx
@@ -1,17 +1,22 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { getCoinDetails, getCoinPrice } from "../../services/coins-api";
+import * as coinService from "../../services/coins-api";
 import { Link } from "react-router-dom";
 const CoinDetails = () => {
   const [coinDetail, setCoinDetail] = useState([])
   const location = useLocation()
   useEffect(() => {
     const fetchCoinsDetail = async () => {
-      const coinData = await getCoinDetails(location.state.coin.id)
+      const coinData = await coinService.getCoinDetails(location.state.coin.id)
       setCoinDetail(coinData.data)
     }
     fetchCoinsDetail()
   }, [location.state.coin.id])
+
+  const handleAddCoin = async () => {
+    const coinData = await coinService.addCoinToWallet(coinDetail)
+  }
+
   return (
     <div>
       <h2>Coin Details</h2>
@@ -19,8 +24,7 @@ const CoinDetails = () => {
         <p>Rank: {coinDetail.rank}</p>
         <p>Symbol: {coinDetail.symbol}</p>
         <p>Price: ${coinDetail.priceUsd}</p>
-        <p></p>
-        <p>{}</p>
+        <button onClick={() => handleAddCoin()}>Add to wallet</button>
       </div>
       
     </div>
--- src/services/coins-api.js
+++ src/services/coins-api.js
@@ -1,3 +1,4 @@
+import * as tokenService from './tokenService'
 const baseURL = 'https://api.coincap.io/v2'
 export async function getAllCoins() {
   const res = await fetch(`${baseURL}/assets`)
@@ -9,7 +10,18 @@ export async function getCoinDetails(coinId) {
   return res.json()
 }
 
-export async function getCoinPrice(coinId) {
-  const res = await fetch(`${baseURL}/assets/${coinId}/history?interval=d1`)
-  return res.json()
+export async function addCoinToWallet(coinDetails) {
+  try {
+    const res = await fetch(baseURL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(coinDetails)
+    })
+    return res.json()
+  } catch(err) {
+    console.log(err)
+  }
 }
