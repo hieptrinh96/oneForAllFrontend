bd8ffd6d86c46a27de4a5c326dd4e6f93f59eca5
hieptrinh96 <xmikecolx@gmail.com>
2023-01-24 17:34:27 -0600

    add condition to check if the user is logged in

--- src/pages/CoinDetails/CoinDetails.jsx
+++ src/pages/CoinDetails/CoinDetails.jsx
@@ -1,16 +1,20 @@
 import { useState, useEffect } from "react"
 import { useLocation } from "react-router-dom"
-const CoinDetails = (props) => {
+const CoinDetails = (props, {user}) => {
   const { state } = useLocation()
   
   return (
     <div>
+      {user ?
       <div>
         <p>Rank: {state.rank}</p>
         <p>Symbol: {state.symbol}</p>
         <p>Price: ${state.priceUsd}</p>
         <button onClick={() => props.handleAddCoin(state)}>Save Coin</button>
       </div>
+      :
+      <h1>Please log in to access all the features!</h1>
+      }
     </div>
   );
 }
