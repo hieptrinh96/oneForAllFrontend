89bd03eb24405446e1bc386ce384af95b91ed311
hieptrinh96 <xmikecolx@gmail.com>
2023-01-29 14:57:49 -0600

    style navbar layout

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,15 +5,15 @@ const NavBar = ({ user, handleLogout }) => {
     <nav>
       {user ?
         <ul className={styles['loggedin-container']}>
-          <li><Link to='/myWallet'>My Wallet</Link></li>
-          <li><Link to='/coins'>All Coins</Link></li>
-          <li><Link to="/changePassword">Change Password</Link></li>
-          <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+          <h2><li><Link to='/myWallet'>My Wallet</Link></li></h2>
+          <h2><li><Link to='/coins'>All Coins</Link></li></h2>
+          <h2><li><Link to="/changePassword">Change Password</Link></li></h2>
+          <h2><li><Link to="" onClick={handleLogout}>LOG OUT</Link></li></h2>
         </ul>
       :
         <ul className={styles['logout-container']}>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
+          <h2><li><Link to="/login">Log In</Link></li></h2>
+          <h2><li><Link to="/signup">Sign Up</Link></li></h2>
         </ul>
       }
     </nav>
--- src/pages/MyWallet/MyWallet.jsx
+++ src/pages/MyWallet/MyWallet.jsx
@@ -1,14 +1,25 @@
+import styles from './MyWallet.module.css'
 const MyWallet = ({coins}) => {
-
   return (
     <>
-    <p>My Coins</p>
+    <div className={styles.headerContainer}>
+      <h2>Rank</h2>
+      <h2>Symbol</h2>
+      <h2>Name</h2>
+      <h2>Price</h2>
+      <h2>Supply</h2>
+      <h2>Max Supply</h2>
+    </div>
     <div>
       {coins.map(coin => 
         <div key={coin.id}>
-          <div>
+          <div className={styles.coinContainer}>
           <p>{coin.rank}</p>
+          <p>{coin.symbol}</p>
           <p>{coin.name}</p>
+          <p>{coin.priceUsd}</p>
+          <p>{coin.supply}</p>
+          <p>{coin.maxSupply}</p>
           </div>
         </div>  
       )}
